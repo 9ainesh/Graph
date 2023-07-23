758a29a4a9aa3f171ed7fb8bd36005c06e255b40
9ainesh <92214634+9ainesh@users.noreply.github.com>
2023-07-23 17:13:32 +0530

    Add files via upload

---
+++ DFS.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(v) v.begin(),v.end()
+const int mx=1e3+6;
+
+vector<int>graph[mx];
+int height[mx];
+int depth[mx];
+bool vis[mx];
+
+vector< vector<int> >compo;
+vector<int>tmp;
+
+
+
+void dfs(int node)
+{
+	vis[node]=true;
+	//cout<<node<<" ";
+	tmp.push_back(node);
+	for(auto ele:graph[node])
+	{
+		if(vis[ele])
+			continue;
+
+		depth[ele]=depth[node]+1;
+		dfs(ele);
+		height[node]=max(height[ele]+1,height[node]);
+	}
+    
+}
+
+
+int main()
+{
+	ll n,m,i,j;
+	cin>>n>>m;
+	memset(vis,false,sizeof(vis));
+	while(m--)
+	{
+		cin>>i>>j;
+		graph[i].push_back(j);
+		graph[j].push_back(i);
+	}
+
+	for(int i=1;i<=n;i++)
+	{
+		if(vis[i])
+			continue;
+		tmp.clear();
+		dfs(i);
+		compo.push_back(tmp);
+	}
+
+	for(i=1;i<=n;i++)
+	{
+		cout<<height[i]<<" "<<depth[i]<<"\n";
+	}
+
+	cout<<"\ncompo: "<<compo.size()<<"\n";
+	for(auto ele:compo)
+	{
+		for(auto el:ele)
+		{
+			cout<<el<<" ";
+		}
+		cout<<"\n";
+	}
+
+}
